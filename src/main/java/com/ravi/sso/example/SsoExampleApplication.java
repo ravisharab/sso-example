6b70683d201ca7c3737bb16de8266414f878b234
ravisharab <ravisharab@gmail.com>
2025-08-28 17:56:14 -0400

    enable method security enabled

--- src/main/java/com/ravi/sso/example/SsoExampleApplication.java
+++ src/main/java/com/ravi/sso/example/SsoExampleApplication.java
@@ -2,8 +2,10 @@ package com.ravi.sso.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
+@EnableMethodSecurity
 public class SsoExampleApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/ravi/sso/example/UserController.java
+++ src/main/java/com/ravi/sso/example/UserController.java
@@ -1,5 +1,6 @@
 package com.ravi.sso.example;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @GetMapping("/user")
+    @PreAuthorize("hasAuthority('SCOPE_readUser')")
     public String getUser(){
         return "Hi User";
     }
