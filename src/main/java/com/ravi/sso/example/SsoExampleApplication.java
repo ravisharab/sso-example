431c3f1981e2677c652499b44b2137b60d911054
ravisharab <ravisharab@gmail.com>
2025-08-28 17:48:52 -0400

    Config changes for sso jwt for auth0

--- src/main/java/com/ravi/sso/example/SsoExampleApplication.java
+++ src/main/java/com/ravi/sso/example/SsoExampleApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SsoExampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SsoExampleApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(SsoExampleApplication.class, args);
 
-	}
+    }
 
 }
