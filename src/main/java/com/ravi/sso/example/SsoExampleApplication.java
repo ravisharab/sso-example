01650a8a470fa1eac6a502ce3b608323a95d3821
ravikumarganta <ravisharab@gmail.com>
2025-08-28 15:38:38 -0400

    Initial commit

---
+++ src/main/java/com/ravi/sso/example/SsoExampleApplication.java
@@ -0,0 +1,13 @@
+package com.ravi.sso.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsoExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ravi/sso/example/SsoExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravi.sso.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsoExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
